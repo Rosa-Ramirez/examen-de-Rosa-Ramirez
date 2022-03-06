f65d00eec0f95e4b4291461a2fd70d8c425c610b
Rosa-Ramirez <rramireze5@miumg.edu.gt>
2022-03-06 14:34:12 -0600

    Se agregaron las variables y la funcion de ingresar clave

--- examen-de-Rosa-Ramirez.cpp
+++ examen-de-Rosa-Ramirez.cpp
@@ -7,7 +7,25 @@ int tabla(int a);
 
 int main()
 {
+	int clave, container = 0, accion, contador_par = 0, contador_impar = 0, prom = 0, sumapar, sumaimpar, dia, a;
 	
+	cout << "---------- Parcial de Programacion ----------" << endl;
+	cout << "Hola, para poder ingresar al sistema debe ingresar la clave correctamente." << endl;
+	
+	while (clave != 123456)
+	{
+		cout << "Ingrese la clave: ";
+		cin >> clave;
+		
+		container = container + 1;
+		if (container == 3)
+		{
+		
+		cout << "\nLo siento, usted ha ingresado la clave incorrecta en 3 oportunidades, su ingreso no es permitido." << endl;
+		break;
+		}
+
+	}
 	
 	
 	return 0;
